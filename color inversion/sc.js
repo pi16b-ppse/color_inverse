40cf819f26678cbf1b26095025f6ac12a1a4cc74
igoryok981 <36260597+igoryok981@users.noreply.github.com>
2019-04-05 17:07:09 +0300

    Работа по нажатию одной кнопки

--- color inversion/sc.js
+++ color inversion/sc.js
@@ -1,10 +1,18 @@
 var css = 'html {background-color: #f00;' + '-webkit-filter: invert(100%);' + 'filter: invert(100%); }';	
+var css2 = 'html {background-color: #fff;' + '-webkit-filter: invert(0%);' + 'filter: invert(0%); }';
 var style = document.createElement('style');
+var el = document.getElementsByTagName("style");
 
 style.type = 'text/css';
 if (style.styleSheet) {
     style.styleSheet.cssText = css;
 } else {
-    style.appendChild(document.createTextNode(css));
+    if (el[el.length - 1].id === '1') {	
+        style.id = '0';	
+        style.appendChild(document.createTextNode(css2));		
+    } else {	
+        style.id = '1';		
+        style.appendChild(document.createTextNode(css));	
+    }
 }
 document.documentElement.appendChild(style);
