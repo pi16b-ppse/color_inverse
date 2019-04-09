7a5f9136fc3ac21f0859b77169da6c075e2d4154
igoryok981 <36260597+igoryok981@users.noreply.github.com>
2019-04-09 10:55:27 +0300

    Изменение иконки в зависимости от цвета страницы

--- color inversion/sc.js
+++ color inversion/sc.js
@@ -1,4 +1,4 @@
-var css = 'html {background-color: #f00;' + '-webkit-filter: invert(100%);' + 'filter: invert(100%); }';	
+var css = 'html {background-color: #fff;' + '-webkit-filter: invert(100%);' + 'filter: invert(100%); }';	
 var css2 = 'html {background-color: #fff;' + '-webkit-filter: invert(0%);' + 'filter: invert(0%); }';
 var style = document.createElement('style');
 var el = document.getElementsByTagName("style");
@@ -7,12 +7,18 @@ style.type = 'text/css';
 if (style.styleSheet) {
     style.styleSheet.cssText = css;
 } else {
-    if (el[el.length - 1].id === '1') {	
-        style.id = '0';	
-        style.appendChild(document.createTextNode(css2));		
+	if (el.length == 0) {
+		style.id = '0';		
+        style.appendChild(document.createTextNode(css2));
+		document.documentElement.appendChild(style);
+	}
+    if (el[el.length - 1].id === '0') {	
+        style.id = '1';	
+        style.appendChild(document.createTextNode(css));		
     } else {	
-        style.id = '1';		
-        style.appendChild(document.createTextNode(css));	
+        style.id = '0';		
+        style.appendChild(document.createTextNode(css2));		
     }
 }
 document.documentElement.appendChild(style);
+chrome.extension.sendMessage(style.id);
--- color inversion/scc.js
+++ color inversion/scc.js
@@ -1,6 +1,11 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
     chrome.tabs.executeScript(null, {
         file: "sc.js"
-    }); 
+    });
 });
-
+chrome.extension.onMessage.addListener(function(request){
+	if (request == '1')
+		chrome.browserAction.setIcon({path: "icon2.png"});
+	else
+		chrome.browserAction.setIcon({path: "icon.png"});
+});
