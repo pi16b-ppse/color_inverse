a38e999f84c6a4aad5c8cc1f04ccee87c41c8e1c
igoryok981 <36260597+igoryok981@users.noreply.github.com>
2019-04-24 12:15:58 +0300

    Изменение иконки

--- color inversion/scc.js
+++ color inversion/scc.js
@@ -3,3 +3,11 @@ chrome.browserAction.onClicked.addListener(function(tab) {
         chrome.tabs.executeScript(null, {file:"sc.js"});
     });
 });
+chrome.extension.onMessage.addListener(function(request){
+    if (request == true) {
+        chrome.browserAction.setIcon({path: "icon2.png"});
+    } 
+    else {
+        chrome.browserAction.setIcon({path: "icon.png"});
+    }
+});
