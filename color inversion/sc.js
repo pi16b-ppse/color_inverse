1010f219c069694d7143c460b204d445ccf7bdbe
igoryok981 <36260597+igoryok981@users.noreply.github.com>
2019-05-06 13:16:44 +0300

    Обновление кода и иконки

--- color inversion/sc.js
+++ color inversion/sc.js
@@ -1,6 +1,10 @@
-if(!$("html").hasClass('invex')) {
+function create_classes() {
     $("<style type='text/css'> .inv{background-color: #fff; -webkit-filter: invert(100%); filter: invert(100%); } </style>").appendTo("html");
     $("html").attr('class', 'invex');
+}
+
+if(!$("html").hasClass('invex')) {
+    create_classes();
 }
 $("html").toggleClass('inv');
 chrome.extension.sendMessage($("html").hasClass('inv'));
--- color inversion/scc.js
+++ color inversion/scc.js
@@ -1,3 +1,6 @@
+const PATH1 = "icon.png";
+const PATH2 = "icon2.png";
+
 chrome.browserAction.onClicked.addListener(function(tab) {
     chrome.tabs.executeScript(null, {file:"jquery.js"}, function() {
         chrome.tabs.executeScript(null, {file:"sc.js"});
@@ -5,9 +8,9 @@ chrome.browserAction.onClicked.addListener(function(tab) {
 });
 chrome.extension.onMessage.addListener(function(request){
     if (request == true) {
-        chrome.browserAction.setIcon({path: "icon2.png"});
-    } 
+        chrome.browserAction.setIcon({path: PATH2});
+    }
     else {
-        chrome.browserAction.setIcon({path: "icon.png"});
+        chrome.browserAction.setIcon({path: PATH1});
     }
 });
