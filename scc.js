65fbb8bb39639fb15f2c87ea42ab2f2e3aa4686d
igoryok981 <36260597+igoryok981@users.noreply.github.com>
2019-05-22 12:47:59 +0300

    Изменение названия пути к файлу

---
+++ scc.js
@@ -0,0 +1,26 @@
+/**
+ * Путь к первой иконке 
+ *
+ * @const {string}
+ */
+const ICON_PATH1 = "icon.png";
+/**
+ * Путь ко второй иконке 
+ *
+ * @const {string}
+ */
+const ICON_PATH2 = "icon2.png";
+
+chrome.browserAction.onClicked.addListener(function(tab) {
+    chrome.tabs.executeScript(null, {file:"jquery.js"}, function() {
+        chrome.tabs.executeScript(null, {file:"sc.js"});
+    });
+});
+chrome.extension.onMessage.addListener(function(request){
+    if (request == true) {
+        chrome.browserAction.setIcon({path: ICON_PATH2});
+    }
+    else {
+        chrome.browserAction.setIcon({path: ICON_PATH1});
+    }
+});
